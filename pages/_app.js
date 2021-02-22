68c53fa01198467b1daef120f415f9895aea1ea9
Patty Ouwehand <Patty@g2g.local>
2021-02-22 09:18:25 +0100

    Added debugger at project and footer at homepage

--- pages/_app.js
+++ pages/_app.js
@@ -8,23 +8,25 @@ import Navbar from '@/components/shared/Navbar'
 
 const MyApp = ({Component, pageProps}) => {
 
+  const isHomePage = () => Component.name === 'Home'
+
   return (
     <div className="portfolio-app">
       <Navbar />
-      {pageProps.appData}
-      {Component.name === 'Home' && <Hero />}
+      { isHomePage() && <Hero /> }
       <div className="container">
         <Component {...pageProps} />
+
+        {isHomePage() &&
+          <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+            <div className="container text-center">
+              <small>Copyright &copy; Your Website</small>
+            </div>
+          </footer>
+        }
       </div>
     </div>
   )
-}
-
-MyApp.getInitialProps = async (context) => {
-  console.log('GET INITIAL PROPS _APP')
-  const initialProps = App.getInitialProps && await App.getInitialProps(context);
-
-  return {pageProps: {appData: 'Hello _App Component', ...initialProps.pageProps}}
 }
 
 export default MyApp
